c8bb061a6e00a2d0d8b9b37e92d9212302f466f2
Vasilka Tomanova <vasilkatomanova@gmail.com>
2021-06-01 17:53:57 +0300

    Update StartUp.cs

--- MyWebServer/StartUp.cs
+++ MyWebServer/StartUp.cs
@@ -20,15 +20,41 @@ namespace MyWebServer
             while (true)
             {
                 TcpClient currentConnectionClient = await serverListener.AcceptTcpClientAsync();
-
                 NetworkStream networkStream = currentConnectionClient.GetStream();
 
+                //  REQUEST
+                int bufferLenght = 1024;
+                byte[] buffer = new byte[bufferLenght];
+                StringBuilder streamReaderOfRequest = new StringBuilder();
+
+                //Read information from stream reqest
+                var totalBitesRead = 0;
+                while (networkStream.DataAvailable)
+                {
+                    var bitesRead = await networkStream.ReadAsync(buffer, 0, bufferLenght);
+                    totalBitesRead += bitesRead;
+                    if(totalBitesRead > 10 * 1024)
+                    {
+                        // htrow...
+                        currentConnectionClient.Close(); 
+                    }
+                    streamReaderOfRequest.Append(Encoding.UTF8.GetString(buffer, 0, bitesRead));
+                }
+
+                Console.WriteLine(streamReaderOfRequest.ToString());
+
+                //RESPONSE
                 string responseLine = "HTTP/1.1 200 OK";
-                string responseBody = @"<h2>Hello from *Василка*'s first custom server!<h2>";
+                string responseBody = @"
+<html> <head>
+<link rel=""icon"" href=""data:,"">
+</head> <body>Hello from *Василка*'s first custom server</body> </html>";
                 int responseBodyLenghtOfBytes = Encoding.UTF8.GetByteCount(responseBody);
 
                 //text/html or text/plain ще знае какво да очаква
                 string responce = $@"{responseLine}
+Server: My Custom Web Server for demo purposes
+Date: {DateTime.UtcNow.ToString("r")}
 Content-Length: {responseBodyLenghtOfBytes}
 Content-Type: text/html; charset=UTF-8
 
