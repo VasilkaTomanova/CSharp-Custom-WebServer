9449b35f3bb863fa3fc5c2bad519f5976ae54982
Vasilka Tomanova <vasilkatomanova@gmail.com>
2021-06-01 15:37:07 +0300

    Create StartUp.cs

---
+++ MyWebServer/StartUp.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyWebServer
+{
+   public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
