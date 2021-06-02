406f0cbf2401545819123d6c9e16d0792cc973ec
Vasilka Tomanova <vasilkatomanova@gmail.com>
2021-06-02 17:40:15 +0300

    Update StartUp.cs

--- MyWebServer/StartUp.cs
+++ MyWebServer/StartUp.cs
@@ -3,15 +3,17 @@ using System.Net;
 using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
-
+using MyWebServer.Server;
 namespace MyWebServer
 {
     public class StartUp
     {
         public static async Task Main(string[] args)
         {
-            //Server server = new Server("127.0.0.1", 1234);
-           
+            CustomHttpServer server = new CustomHttpServer("127.0.0.1", 1234);
+            await server.Start();
+
+
         }
     }
 }
