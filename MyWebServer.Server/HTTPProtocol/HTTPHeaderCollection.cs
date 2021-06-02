dbc62a0315e9300e5baa155cb9389f797d5f3ca6
Vasilka Tomanova <vasilkatomanova@gmail.com>
2021-06-02 18:01:33 +0300

    Create HTTPHeaderCollection.cs

---
+++ MyWebServer.Server/HTTPProtocol/HTTPHeaderCollection.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyWebServer.Server.HTTPProtocol
+{
+    public class HTTPHeaderCollection
+    {
+        private readonly Dictionary<string, HTTPHeader> headers;
+        public HTTPHeaderCollection()
+        {
+            this.headers = new Dictionary<string, HTTPHeader>();
+        }
+    }
+}
