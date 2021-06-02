9d9aef58d2b6f8e5e0da392e533fdb64c4776d22
Vasilka Tomanova <vasilkatomanova@gmail.com>
2021-06-02 17:44:31 +0300

    Create HTTPHeader.cs

---
+++ MyWebServer.Server/HTTPProtocol/HTTPHeader.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyWebServer.Server.HTTPProtocol
+{
+   public class HTTPHeader
+    {
+        public string Name { get;private set; }
+        public string Value{ get; private set; }
+
+    }
+}
