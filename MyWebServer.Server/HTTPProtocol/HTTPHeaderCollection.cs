f1732012087d2236ce2c432e055f15b5f0757cba
Vasilka Tomanova <vasilkatomanova@gmail.com>
2021-06-02 19:05:13 +0300

    Update HTTPHeaderCollection.cs

--- MyWebServer.Server/HTTPProtocol/HTTPHeaderCollection.cs
+++ MyWebServer.Server/HTTPProtocol/HTTPHeaderCollection.cs
@@ -6,12 +6,20 @@ using System.Threading.Tasks;
 
 namespace MyWebServer.Server.HTTPProtocol
 {
-    public class HTTPHeaderCollection
+    public class HTTPHeaderCollection 
     {
         private readonly Dictionary<string, HTTPHeader> headers;
         public HTTPHeaderCollection()
         {
             this.headers = new Dictionary<string, HTTPHeader>();
         }
+
+        public void Add(HTTPHeader header)
+        {
+            this.headers.Add(header.Name,header);
+        }
+
+        public int Count => this.headers.Count;
+
     }
 }
